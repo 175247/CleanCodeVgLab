225a7a9bbe37c0218d9cfa90aaecf2c7e48bfd0f
Alexander Molnar <molex90@gmail.com>
2020-12-08 22:57:20 +0100

    Init commit

---
+++ CleanCodeLab3Test/CleanCodeLab3Test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CleanCodeLab3Test
+{
+    [TestClass]
+    public class CleanCodeLab3Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
